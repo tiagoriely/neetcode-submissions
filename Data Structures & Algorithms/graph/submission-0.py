5685e93cedf7f13fd312fcc231498916c1480320
tiagoriely <tiagoriely@gmail.com>
2026-04-12 21:47:36 +0100

    Add: graph - submission-0

---
+++ Data Structures & Algorithms/graph/submission-0.py
@@ -0,0 +1,58 @@
+class Graph:
+    
+    def __init__(self):
+        self.adj_list = {}
+
+
+    def addEdge(self, src: int, dst: int) -> None:
+        # If src or dst don't exist, add them
+        if src not in self.adj_list:
+            self.adj_list[src] = set()
+        if dst not in self.adj_list:
+            self.adj_list[dst] = set()
+        # Add edge if not already exists
+        self.adj_list[src].add(dst)
+
+
+    def removeEdge(self, src: int, dst: int) -> bool:
+        # Check if src and dst exist in the graph
+        if src not in self.adj_list or dst not in self.adj_list[src]:
+            return False
+        
+        # Remove the edge
+        self.adj_list[src].remove(dst)
+        return True
+
+
+
+    def hasPath(self, src: int, dst: int) -> bool:
+        
+        visit = set()
+        return self.dfs(src, dst, visit)
+        
+    def dfs(self, src, dst, visit):
+        if src == dst:
+            return True
+        visit.add(src)
+
+        for neighbor in self.adj_list.get(src, []):
+            if neighbor not in visit:
+                if self.dfs(neighbor, dst, visit):
+                    return True
+        return False
+
+
+    # Alternatively, use BFS for hasPath
+    # def hasPathBFS(self, src: int, dst: int) -> bool:
+    #     visited = set()
+    #     queue = deque([src])
+    #     while queue:
+    #         curr = queue.popleft()
+    #         if curr == dst:
+    #             return True
+    #         visited.add(curr)
+    #         for neighbor in self.adj_list.get(curr, []):
+    #             if neighbor not in visited:
+    #                 queue.append(neighbor)
+    #                 visited.add(neighbor)
+    #     return False
