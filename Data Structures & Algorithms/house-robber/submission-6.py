75beb78bd36dd23949c46366493426ae5f5ecea0
tiagoriely <tiagoriely@gmail.com>
2026-05-05 13:04:38 +0100

    Add: house-robber - submission-6

---
+++ Data Structures & Algorithms/house-robber/submission-6.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        cache = {}
+        def dfs(i):
+            if i >= len(nums):
+                return 0
+            if i in cache:
+                return cache[i]
+                
+            cache[i] = nums[i] + dfs(i + 2)
+            return max(dfs(i + 1), cache[i]) # max(skip the house, rob the house)           
+        
+        return dfs(0)
