365a46555374d3354d9a33edc8d78f649db9286c
tiagoriely <tiagoriely@gmail.com>
2026-05-06 09:28:10 +0100

    Add: climbing-stairs - submission-10

---
+++ Data Structures & Algorithms/climbing-stairs/submission-10.py
@@ -0,0 +1,11 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = [1, 1]
+
+        for i in range(2, n + 1):
+            tmp = dp[1]
+            dp[1] = dp[0] + dp[1]
+            dp[0] = tmp
+        
+        return dp[1]
+        
