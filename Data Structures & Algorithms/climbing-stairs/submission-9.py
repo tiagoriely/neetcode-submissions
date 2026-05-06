d81240fd1624a68d1620363509e1445357da42bc
tiagoriely <tiagoriely@gmail.com>
2026-05-06 09:19:35 +0100

    Add: climbing-stairs - submission-9

---
+++ Data Structures & Algorithms/climbing-stairs/submission-9.py
@@ -0,0 +1,15 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        cache = {}
+        def dfs(i):
+            if i > n:
+                return 0
+            if i == n:
+                return 1
+            
+            if i in cache:
+                return cache[i]
+            cache[i] = dfs(i + 1) + dfs(i + 2)
+            return cache[i]
+            
+        return dfs(0)
