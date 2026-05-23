c92f2d5430390ad28543c02121377a71c36616ee
tiagoriely <tiagoriely@gmail.com>
2026-05-23 21:15:48 +0100

    Add: two-integer-sum - submission-18

---
+++ Data Structures & Algorithms/two-integer-sum/submission-18.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+        int n = nums.size();
+        unordered_map<int, int> seen;
+        for (int i = 0; i < n; i++) {
+            int diff = target - nums[i];
+            auto it = seen.find(diff);
+            if (it != seen.end()) {
+                return {it->second, i};
+            }
+
+            seen.insert({nums[i], i});
+
+        }
+        return {};
+    }
+};
