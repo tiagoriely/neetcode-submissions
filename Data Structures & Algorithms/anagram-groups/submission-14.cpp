2f311381249d53324dc3d645b894dc0f301287ec
tiagoriely <tiagoriely@gmail.com>
2026-05-24 16:19:30 +0100

    Add: anagram-groups - submission-14

---
+++ Data Structures & Algorithms/anagram-groups/submission-14.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> res;
+        for(const auto& s: strs) {
+            string sortedS = s;
+            sort(sortedS.begin(), sortedS.end());
+            res[sortedS].push_back(s);
+        }
+
+        vector<vector<string>> result;
+        for (const auto& pair: res) {
+            result.push_back(pair.second);
+        }
+        return result;
+    }
+};
