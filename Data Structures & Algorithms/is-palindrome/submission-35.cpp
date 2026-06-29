d1dd29fc712929c2b59b7deccf5277a58d45ea1b
tiagoriely <tiagoriely@gmail.com>
2026-06-29 16:45:34 +0100

    Add: is-palindrome - submission-35

---
+++ Data Structures & Algorithms/is-palindrome/submission-35.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int n = s.size();
+        int left{0}, right{n - 1};
+
+        while (left < right && left < n) {
+            while (left < right && !isalnum(s[left]))
+                left++;
+            while (right > left && !isalnum(s[right]))
+                right--;
+            
+            if (tolower(s[left]) != tolower(s[right]))
+                return false;
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+};
